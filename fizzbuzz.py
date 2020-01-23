811bf810ca9b5f1f431f17eae7e05888bd7110e6
nmhk <football.nh0430@icloud.com>
2020-01-24 07:24:22 +0900

    5の倍数のfizzbuzzを追加

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,9 @@
 def fizzbuzz_convert(number):
     if number % 3 == 0:
         return 'Fizz'
+    if number % 5 == 0:
+        return 'Buzz'
 
 
 assert fizzbuzz_convert(3) == 'Fizz'
+assert fizzbuzz_convert(5) == 'Buzz'
